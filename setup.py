19ffecf5fbbce2643769a6cc0be83accf2f74f52
Lorena <lorena.pantano@gmail.com>
2015-08-13 16:50:32 -0400

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 
 
 setup(name='bcbreport',
-      version='0.99.5',
+      version='0.99.6',
       description='report templates for bcbio analysis.',
       # long_description=readme(),
       classifiers=[
