d42311f358d64608a035b6dc48de27a52add2d38
Lorena <lorena.pantano@gmail.com>
2015-08-14 12:46:37 -0400

    fix typo in GC code line

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 
 
 setup(name='bcbreport',
-      version='0.99.8',
+      version='0.99.9',
       description='report templates for bcbio analysis.',
       # long_description=readme(),
       classifiers=[
