53ba6985ff335097059140cd64ed33166231d0c8
Lorena <lorena.pantano@gmail.com>
2015-09-28 15:18:45 -0400

    Fix GC plot

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 
 
 setup(name='bcbreport',
-      version='0.99.13',
+      version='0.99.14',
       description='report templates for bcbio analysis.',
       # long_description=readme(),
       classifiers=[
