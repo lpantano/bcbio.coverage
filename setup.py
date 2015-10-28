4604fb041c4f4c0d435fb7080509b1d2823bd344
Lorena <lorena.pantano@gmail.com>
2015-10-28 17:13:18 -0400

    Ignore extra coverage files. Print only if columns exist

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 
 
 setup(name='bcbreport',
-      version='0.99.15',
+      version='0.99.16',
       description='report templates for bcbio analysis.',
       # long_description=readme(),
       classifiers=[
