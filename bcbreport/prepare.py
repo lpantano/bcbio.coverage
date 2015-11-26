ddce55cc3c59abfba2cafd4d66f95cf62b6ab234
Lorena <lorena.pantano@gmail.com>
2015-11-26 10:52:59 -0500

    get current directory

--- bcbreport/prepare.py
+++ bcbreport/prepare.py
@@ -13,15 +13,14 @@ def _get_template(temp):
     template =  op.normpath(op.join(op.dirname(op.realpath(__file__)), temp + ".Rmd"))
     return open(template).read()
 
-def report(out_dir):
+def report(out_dir=None):
     """
     create rmd template
     """
     content = _get_template("header")
-    out_content = string.Template(content).safe_substitute({'path_results': op.abspath(out_dir)})
     out_file = op.join(out_dir, "report-ready.Rmd")
     with open(out_file, 'w') as out_handle:
-        print >>out_handle, out_content
+        print >>out_handle, content
 
         if file_exists(op.join(out_dir, "qsignature.ma")):
             print >>out_handle, _get_template("qsignature")
