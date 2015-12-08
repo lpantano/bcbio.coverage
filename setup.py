5fa0772df43048b2dfd4045d1e80e8aa3fa6a196
Lorena <lorena.pantano@gmail.com>
2015-12-08 16:06:31 -0500

    Fix name files for coverage and other improvements.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 
 
 setup(name='bcbreport',
-      version='0.99.19',
+      version='0.99.20',
       description='report templates for bcbio analysis.',
       # long_description=readme(),
       classifiers=[
