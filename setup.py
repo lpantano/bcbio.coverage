9c37dd6e9555d7bfb6ebeb34520333a1b716ce20
Lorena Pantano <lorena.pantano@gmail.com>
2016-04-19 13:43:16 -0400

    Fix error when depth is not well calcultaed.
    thanks @lbeltrame:
    https://github.com/chapmanb/bcbio-nextgen/issues/1346

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 
 
 setup(name='bcbreport',
-      version='0.99.27',
+      version='0.99.28',
       description='report templates for bcbio analysis.',
       # long_description=readme(),
       classifiers=[
